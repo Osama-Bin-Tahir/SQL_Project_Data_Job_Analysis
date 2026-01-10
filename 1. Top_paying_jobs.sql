d6d6cc18c52158c6bac3a5f6b10ba0b4454c2379
Osama Bin Tahir <otahir@isrt.ac.bd>
2026-01-10 16:15:18 +0600

    push example

---
+++ 1. Top_paying_jobs.sql
@@ -0,0 +1,16 @@
+/*
+-----------------------------------------------------------------------
+Portfolio Project:
+
+Goal:
+ - Analyze the top-paying roles and skills
+ - Create SQL queries to explore this large dataset
+
+
+Questions to Answer:
+1. What are the top-paying jobs for my role?
+2. What are the skills required for these top-paying jobs?
+3. What are the most in-demand skills for my role?
+4. What are the top skills based on salary for my role?
+5. What are the most optimal skills to learn? (High demand + High paying)
+*/
